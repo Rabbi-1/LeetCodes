1e32925c04471023a22eeac552917e77fdd9c6d8
Rabbi-1 <fuadhrabbi66@gmail.com>
2025-08-01 22:02:41 -0400

    21. Merge Two Sorted Lists

---
+++ Leetcode/App/LinkedList/MergeTwoLinkedList.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head1 = list1
+        head2 = list2
+        dummy = ListNode(0)
+        tail = dummy
+        while head1 and head2:
+            if head1.val <= head2.val:
+                tail.next = head1
+                head1 = head1.next
+            else:
+                tail.next = head2
+                head2 = head2.next
+            tail = tail.next
+        #if any leftover
+        if head1:
+            tail.next = head1
+        if head2:
+            tail.next = head2
+        return dummy.next
+
+#21. Merge Two Sorted Lists
