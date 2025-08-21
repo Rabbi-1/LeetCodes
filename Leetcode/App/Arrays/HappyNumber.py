c233dd3a2579add90732615cd1378c2114a6b507
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-08-20 20:49:21 -0400

    202. Happy Number

--- Leetcode/App/Arrays/HappyNumber.py
+++ Leetcode/App/Arrays/HappyNumber.py
@@ -7,8 +7,15 @@ def isHappy(n: int) -> bool:
         n = sum([int(i) ** 2 for i in str(n)])
     return True
 
+def isHappy(self, n: int) -> bool:
+    def nxt(x: int) -> int:
+        return sum((int(i) ** 2 for i in str(x)))
+    slow = n
+    fast = nxt(n)
+    while fast != 1 and slow != 1 and slow != fast:
+        slow = nxt(slow)
+        fast = nxt(nxt(fast))
 
-class Solution:
-    pass
+    return fast == 1 or slow == 1
 
 #202. Happy Number
