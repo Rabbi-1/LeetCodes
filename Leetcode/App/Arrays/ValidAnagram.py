a0a0ee601fddf560ce0fb4d298c99b8be554ceee
Rabbi-1 <fuadhrabbi66@gmail.com>
2025-08-21 21:50:24 -0400

    Leetcode 242

--- Leetcode/App/Arrays/ValidAnagram.py
+++ Leetcode/App/Arrays/ValidAnagram.py
@@ -3,4 +3,16 @@ class Solution:
         sorted_s = ''.join(sorted(s))
         sorted_t = ''.join(sorted(t))
         return sorted_s == sorted_t
+
+    def isAnagram2(self, s: str, t: str) -> bool:
+        count = [0] * 26
+
+        for i in s:
+            count[ord(i) - ord('a')] += 1
+        for i in t:
+            count[ord(i) - ord('a')] -= 1
+        for val in count:
+            if val != 0:
+                return False
+        return True
 #Leetcode 242
