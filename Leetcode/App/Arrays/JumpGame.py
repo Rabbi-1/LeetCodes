58781ca7ed08d83452228dbd7a559ea3317bb641
Rabbi-1 <fuadhrabbi66@gmail.com>
2025-09-01 02:46:58 -0400

    53. Maximum Subarray

---
+++ Leetcode/App/Arrays/JumpGame.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def canJump(self, nums: List[int]) -> bool:
+    gas = 0
+    for n in nums:
+        if gas < 0: #run out of gas
+            return False
+        elif n > gas:
+            gas = n
+        gas -= 1
+    return True
+#55. Jump Game
+#we go through the array each iteration takes one gas if next index gas is greater replace gas
+#now we do this proccess till we reach the end(true) or we run out(false)
