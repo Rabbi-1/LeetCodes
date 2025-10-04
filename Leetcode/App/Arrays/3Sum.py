9aa40a96301f52f939a5bd2f0394f4a9ecc990df
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-10-04 10:42:19 -0400

    167. Two Sum II - Input Array Is Sorted

--- Leetcode/App/Arrays/3Sum.py
+++ Leetcode/App/Arrays/3Sum.py
@@ -33,3 +33,13 @@ class Solution:
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return res
+'''
+Time complexity: The ti.me complexity of triplet_sum is O(n^2). Here's why:
+• We first sort the array, which takes O(n log(n)) time.
+• Then, for each of then elements in the array, we call pair _sum_sorted_all_pairs at most
+once. which runs in O(n) time.
+Therefore. the overall time complexity is O(n log(n)) + O(n^2) = O(n^2). 
+
+Space complexity: The space complexity jsO(n) due to the space taken up by Python's sorting al·
+gorithm
+'''
