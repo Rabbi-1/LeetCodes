3916aff328504a4fe475222d1a0573a4dbde96b1
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-10-04 11:50:32 -0400

    Container with most water

--- Leetcode/App/Arrays/ContainerWithMostWater.py
+++ Leetcode/App/Arrays/ContainerWithMostWater.py
@@ -12,3 +12,28 @@ class Solution:
                 max_water = max(max_water, water)
 
         return max_water
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        water_max = 0
+        while left < right:
+
+            water = min(height[left], height[right]) * (right - left)
+            water_max = max(water_max, water)
+            if height[right] < height[left]:
+                right -= 1
+            elif height[right] > height[left]:
+                left += 1
+            else:
+                left += 1
+                right -= 1
+        return water_max
+
+
+'''
+Time complexlty: The time complexity of largest_container is O(n) because we perform approximately n iterations using the two-pointer technique.
+Space complexity: We only allocated a constant number of variables, so the space complexity is
+0(1)
+'''
