5d55d155e3e1a790f034b6f57c2967a030913aae
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-11-03 19:04:49 -0500

    TopKFrequentElements.py

--- Leetcode/App/Arrays/TopKFrequentElements.py
+++ Leetcode/App/Arrays/TopKFrequentElements.py
@@ -17,3 +17,17 @@ class Solution:
         while len(res) < k:
             res.append(arr.pop()[1])
         return res
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq =[[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
